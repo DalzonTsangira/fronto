44d6b232cee37dfd96ac08100894c68f6fde9bef
DalzonTsangira <dalzondalzy@gmail.com>
2020-11-07 23:06:28 +0100

    after modification of vente by adding create new vente

--- src/controllers/ventesController.js
+++ src/controllers/ventesController.js
@@ -9,3 +9,29 @@ exports.getProductInVente=async (req,res)=>{
       return res.status(500).send({error});
     }
 }
+
+exports.createVente=async (req,res)=>{
+  const {monnaie,productId,phone,qty,name}=req.body
+  try {
+      const product=await db.ventes.create({
+        data:{
+          monnaie,
+          products:{
+            connect:{
+              id:productId
+            }
+          },
+          total:qty ,
+          users:{
+            create:{
+              full_name:name,
+              phone
+            }
+          }
+        }
+      });
+      return res.status(200).send(product);
+  } catch (error) {
+    return res.status(500).send({error});
+  }
+}
